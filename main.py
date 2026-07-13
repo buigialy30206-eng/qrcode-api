1fea5184ded9fe2e97d3269217f4ce1141a79c4d
buigialy30206-eng <buigialy30206@gmail.com>
2026-07-13 11:57:57 +0800

    add rate limit middleware

--- main.py
+++ main.py
@@ -8,31 +8,13 @@ import base64
 import io
 
 import qrcode
-from fastapi import FastAPI, Depends, Query
+from fastapi import Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response, JSONResponse
 
-import time as _t, threading as _th
-_rl_win, _rl_max, _rl_hits, _rl_lk = 60, 60, {}, _th.Lock()
-
-async def _rate_limit(request):
-    from fastapi import Request, HTTPException
-    ip = (request.headers.get('X-Forwarded-For','') or request.headers.get('X-Real-IP','') or (request.client.host if request.client else '127.0.0.1')).split(',')[0].strip()
-    now = _t.time()
-    with _rl_lk:
-        e = _rl_hits.get(ip)
-        if e:
-            if now - e['s'] > _rl_win: e['s'], e['c'] = now, 1
-            else:
-                e['c'] += 1
-                if e['c'] > _rl_max: raise HTTPException(429, 'Too many requests')
-        else: _rl_hits[ip] = {'s': now, 'c': 1}
-    return True
-
 app = FastAPI(title="QR Code Generator API", version="1.0.0")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
-
-
+app.add_middleware(RateLimitMiddleware)
 
 
 
@@ -40,7 +22,6 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 @app.get("/")
 async def root():
     return {"service": "QR Code Generator API", "version": "1.0.0"}
-
 
 @app.get("/generate")
 async def generate(
